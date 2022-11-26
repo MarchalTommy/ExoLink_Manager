7dae058e7db0eaae3df9bb80fec5577ba19c5040
Tommy Marchal <tmarchal@exomind.fr>
2022-11-26 18:34:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/exolinkmanager/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.exolinkmanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFFFED766)
+val PrimaryLight = Color(0xFFffff97)
+val PrimaryDark = Color(0xFFc8a636)
+val PrimaryText = Color(0xFF6b4d07)
+
+val Secondary = Color(0xFF009fb7)
+val SecondaryLight = Color(0xFF5ad0e9)
+val SecondaryDark = Color(0xFF007087)
+val SecondaryText = Color(0xFF09343a)
+
+val Surface = Color(0xFFEFF1F3)
+val SurfaceText = Color(0xFF4E4C54)
