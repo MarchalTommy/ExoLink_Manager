f6565f73c7e16514f5567db41ceaf000477d2820
Tommy Marchal <tmarchal@exomind.fr>
2023-09-22 15:01:04 +0200

    Updated Android Gradle Plugin + accelerated build time

--- app/src/main/java/com/example/exolinkmanager/ui/components/CardsScreen.kt
+++ app/src/main/java/com/example/exolinkmanager/ui/components/CardsScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -20,7 +19,6 @@ import com.example.exolinkmanager.utils.dp
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 // TODO: Manage favorite state of the items
-@OptIn(ExperimentalMaterial3Api::class)
 @ExperimentalCoroutinesApi
 @Composable
 fun CardsScreen(
@@ -53,7 +51,7 @@ fun CardsScreen(
                             viewModel.setSelectedCardId(card.id)
                             showEditDialog.value = true
                         },
-                        onFavorite = { /*TODO*/
+                        onFavorite = {
                             viewModel.setSelectedCardId(card.id)
                         },
                         isFavorite = false,
