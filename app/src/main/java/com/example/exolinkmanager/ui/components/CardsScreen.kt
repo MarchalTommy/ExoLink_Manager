1d90d4b4215931a939a99a69ebe3e013c006dcfd
Tommy MARCHAL <tmarchal@exomind.fr>
2024-02-09 16:40:48 +0100

    [REFACT] After full app breakdown -> * Cleaned up CardsScreen by removing duplicated code and simplifying the composable * Cleaned the QuickLinkLaunchBar to remove the useless box

--- app/src/main/java/com/example/exolinkmanager/ui/components/CardsScreen.kt
+++ app/src/main/java/com/example/exolinkmanager/ui/components/CardsScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -20,7 +19,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.exolinkmanager.ui.models.CardModel
@@ -36,7 +34,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 @Composable
 fun CardsScreen(
     parentInnerPadding: PaddingValues,
-    cardViewModel: CardsViewModel = viewModel(),
+    cardViewModel: CardsViewModel=viewModel(),
     onCardClick: ((Deeplink) -> Unit)
 ) {
     val cards by cardViewModel.cards.collectAsState()
@@ -45,144 +43,107 @@ fun CardsScreen(
     val favoritesCardsId by cardViewModel.favoritesDeeplinkList.collectAsState()
     val sortingState by cardViewModel.activeSort.collectAsState()
 
-    val showDeleteDialog = rememberSaveable { mutableStateOf(false) }
-    val showEditDialog = rememberSaveable { mutableStateOf(false) }
+    val showDeleteDialog=rememberSaveable { mutableStateOf(false) }
+    val showEditDialog=rememberSaveable { mutableStateOf(false) }
 
     cardViewModel.getFavoritesDeeplink()
 
-    Scaffold(
-        containerColor = Color.White,
-        modifier = Modifier.padding(parentInnerPadding)
-    ) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            LazyColumn(Modifier.padding(innerPadding)) {
+    LazyColumn(
+        Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(parentInnerPadding)
+    ) {
 
-                // TODO: CLEAN AND REMOVE DUPLICATE CODE
+        item {
+            QuickLinkLaunchBar(onCardClick)
+        }
 
-                if (sortingState == Filters.ALL) {
-                    var filteredCards: List<CardModel>
-                    val headerList = cards.map { it.deeplink.schema }.toSet()
+        val headerList=if (sortingState == Filters.ALL) {
+            cards.map { it.deeplink.schema }.toSet()
+        } else {
+            setOf("")
+        }
 
-                    stickyHeader {
-                        QuickLinkLaunchBar(onCardClick)
-                    }
+        headerList.forEach { schema ->
 
-                    headerList.forEach { schema ->
+            val filteredCards=if (sortingState == Filters.ALL) {
+                cards.filter { it.deeplink.schema == schema }
+            } else {
+                cards
+            }
 
-                        filteredCards = cards.filter { it.deeplink.schema == schema }
+            if (sortingState == Filters.ALL) {
+                stickyHeader {
+                    ListHeader(
+                        text=schema.extractFromSchema()
+                    )
+                }
+            }
 
-                        stickyHeader {
-                            ListHeader(
-                                text = schema.extractFromSchema()
+            items(
+                items=filteredCards,
+                key=CardModel::id
+            ) { card ->
+                Box(
+                    modifier=Modifier
+                        .fillMaxWidth()
+                        .animateItemPlacement(),
+                    contentAlignment=Alignment.CenterStart,
+                ) {
+                    ActionRow(
+                        onDelete={
+                            cardViewModel.setSelectedCardId(card.id)
+                            showDeleteDialog.value=true
+                        },
+                        onEdit={
+                            cardViewModel.setSelectedCardId(card.id)
+                            showEditDialog.value=true
+                        },
+                        onFavorite={
+                            cardViewModel.setSelectedCardId(card.id)
+                            cardViewModel.setFavoriteState(card.deeplink)
+                        },
+                        isFavorite=favoritesCardsId.contains(card.id),
+                        iconSize=56.dp,
+                    )
+                    DraggableCard(card=card,
+                        cardHeight=56.dp,
+                        isRevealed=revealedCardIds.contains(card.id),
+                        cardOffset=(168f).dp(),
+                        onExpand={ cardViewModel.onCardRevealed(cardId=card.id) },
+                        onCollapse={ cardViewModel.onCardHidden(cardId=card.id) },
+                        onClick={
+                            onCardClick.invoke(card.deeplink)
+                            cardViewModel.updateDeeplinkUsedData(
+                                card,
+                                cards
                             )
-                        }
-
-                        items(items = filteredCards, key = CardModel::id) { card ->
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .animateItemPlacement(),
-                                contentAlignment = Alignment.CenterStart,
-                            ) {
-                                ActionRow(
-                                    onDelete = {
-                                        cardViewModel.setSelectedCardId(card.id)
-                                        showDeleteDialog.value = true
-                                    },
-                                    onEdit = {
-                                        cardViewModel.setSelectedCardId(card.id)
-                                        showEditDialog.value = true
-                                    },
-                                    onFavorite = {
-                                        cardViewModel.setSelectedCardId(card.id)
-                                        cardViewModel.setFavoriteState(card.deeplink)
-                                    },
-                                    isFavorite = favoritesCardsId.contains(card.id),
-                                    iconSize = 56.dp,
-                                )
-                                DraggableCard(
-                                    card = card,
-                                    cardHeight = 56.dp,
-                                    isRevealed = revealedCardIds.contains(card.id),
-                                    cardOffset = (168f).dp(),
-                                    onExpand = { cardViewModel.onCardRevealed(cardId = card.id) },
-                                    onCollapse = { cardViewModel.onCardHidden(cardId = card.id) },
-                                    onClick = {
-                                        onCardClick.invoke(card.deeplink)
-                                        cardViewModel.updateDeeplinkUsedData(card, cards)
-                                    }
-                                )
-                            }
-                        }
-                    }
-                } else {
-                    items(items = cards, key = CardModel::id) { card ->
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .animateItemPlacement(),
-                            contentAlignment = Alignment.CenterStart,
-                        ) {
-                            ActionRow(
-                                onDelete = {
-                                    cardViewModel.setSelectedCardId(card.id)
-                                    showDeleteDialog.value = true
-                                },
-                                onEdit = {
-                                    cardViewModel.setSelectedCardId(card.id)
-                                    showEditDialog.value = true
-                                },
-                                onFavorite = {
-                                    cardViewModel.setSelectedCardId(card.id)
-                                    cardViewModel.setFavoriteState(card.deeplink)
-                                },
-                                isFavorite = favoritesCardsId.contains(card.id),
-                                iconSize = 56.dp,
-                            )
-                            DraggableCard(
-                                card = card,
-                                cardHeight = 56.dp,
-                                isRevealed = revealedCardIds.contains(card.id),
-                                cardOffset = (168f).dp(),
-                                onExpand = { cardViewModel.onCardRevealed(cardId = card.id) },
-                                onCollapse = { cardViewModel.onCardHidden(cardId = card.id) },
-                                onClick = {
-                                    onCardClick.invoke(card.deeplink)
-                                    cardViewModel.updateDeeplinkUsedData(card, cards)
-                                }
-                            )
-                        }
-                    }
+                        })
                 }
             }
         }
-
-        cards.find { it.id == selectedCardId }?.deeplink?.let { deeplink ->
-            EditDeeplinkCustomDialog(
-                deeplink = deeplink,
-                showDialog = showEditDialog.value,
-                onConfirm = { modifiedDeeplink ->
-                    cardViewModel.editDeeplink(modifiedDeeplink)
-                    showEditDialog.value = false
-                },
-                onDismiss = { showEditDialog.value = false }
-            )
-        }
-
-        ConfirmationAlertDialog(
-            showDialog = showDeleteDialog.value,
-            onConfirm = {
-                cardViewModel.removeDeeplink(selectedCardId)
-                showDeleteDialog.value = false
-            },
-            onDismiss = { showDeleteDialog.value = false },
-            title = "Delete deeplink",
-            message = "Are you sure you want to delete this deeplink?",
-            icon = Icons.Filled.Warning
-        )
     }
+
+    cards.find { it.id == selectedCardId }?.deeplink?.let { deeplink ->
+        EditDeeplinkCustomDialog(deeplink=deeplink,
+            showDialog=showEditDialog.value,
+            onConfirm={ modifiedDeeplink ->
+                cardViewModel.editDeeplink(modifiedDeeplink)
+                showEditDialog.value=false
+            },
+            onDismiss={ showEditDialog.value=false })
+    }
+
+    ConfirmationAlertDialog(
+        showDialog=showDeleteDialog.value,
+        onConfirm={
+            cardViewModel.removeDeeplink(selectedCardId)
+            showDeleteDialog.value=false
+        },
+        onDismiss={ showDeleteDialog.value=false },
+        title="Delete deeplink",
+        message="Are you sure you want to delete this deeplink?",
+        icon=Icons.Filled.Warning
+    )
 }
--- app/src/main/java/com/example/exolinkmanager/ui/components/QuickLinkLauncher.kt
+++ app/src/main/java/com/example/exolinkmanager/ui/components/QuickLinkLauncher.kt
@@ -1,7 +1,6 @@
 package com.example.exolinkmanager.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,7 +20,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -37,75 +35,81 @@ import com.example.exolinkmanager.ui.viewmodel.CardsViewModel
 @Composable
 fun QuickLinkLaunchBar(
     onClick: (Deeplink) -> Unit,
-    cardViewModel: CardsViewModel = viewModel()
+    cardViewModel: CardsViewModel=viewModel()
 ) {
-    // TODO: CLEAN
-    Box(
-        modifier = Modifier
+    var text by rememberSaveable { mutableStateOf("") }
+    var active by rememberSaveable { mutableStateOf(false) }
+    val deeplinks by cardViewModel.deeplinks.collectAsState()
+
+    DockedSearchBar(
+        modifier=Modifier
             .fillMaxSize()
-            .padding(vertical = dimensionResource(id = R.dimen.margin_large)),
-        contentAlignment = Alignment.Center,
+            .padding(
+                vertical=dimensionResource(id=R.dimen.margin_large),
+                horizontal=dimensionResource(id=R.dimen.margin_xxlarge)
+            ),
+        query=text,
+        onQueryChange={ text=it },
+        onSearch={
+            active=false
+            if (text.isNotBlank() && text.matches("(.*)://(.*)".toRegex())) {
+                text.buildDeeplinkObject("").let {
+                    onClick.invoke(it)
+                }
+            }
+        },
+        active=active,
+        onActiveChange={ active=it },
+        placeholder={ Text(stringResource(id=R.string.quicklink_placeholder)) },
+        leadingIcon={
+            Icon(
+                Icons.Default.Search,
+                contentDescription=null
+            )
+        },
     ) {
-
-        var text by rememberSaveable { mutableStateOf("") }
-        var active by rememberSaveable { mutableStateOf(false) }
-        val deeplinks by cardViewModel.deeplinks.collectAsState()
-
-        DockedSearchBar(
-            modifier = Modifier,
-            query = text,
-            onQueryChange = { text = it },
-            onSearch = {
-                active = false
-                if (text.isNotBlank() && text.matches("(.*)://(.*)".toRegex())) {
-                    text.buildDeeplinkObject("").let {
-                        onClick.invoke(it)
-                    }
-                }
-            },
-            active = active,
-            onActiveChange = { active = it },
-            placeholder = { Text(stringResource(id = R.string.quicklink_placeholder)) },
-            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+        LazyColumn(
+            modifier=Modifier,
         ) {
-            LazyColumn(
-                modifier = Modifier,
-            ) {
-                val items = deeplinks.filter {
-                    it.schema.contains(text, true) ||
-                            it.label.contains(text, true) ||
-                            it.path.contains(text, true
+            val items=deeplinks.filter {
+                it.schema.contains(
+                    text,
+                    true
+                ) || it.label.contains(
+                    text,
+                    true
+                ) || it.path.contains(
+                    text,
+                    true
+                )
+            }
+            items(items=items) { item ->
+                Text(
+                    modifier=Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            onClick.invoke(item)
+                            active=false
+                        }
+                        .padding(
+                            horizontal=dimensionResource(id=R.dimen.margin_large),
+                            vertical=dimensionResource(
+                                id=R.dimen.margin_small
+                            )
+                        ),
+                    text=item.label,
+                    style=MaterialTheme.typography.bodyLarge,
+                    fontWeight=FontWeight.Bold,
+                    color=MaterialTheme.colorScheme.primary,
+                    textAlign=TextAlign.Center,
+                )
+                if (items.size - 1 != (items.indexOf(item))) {
+                    HorizontalDivider(
+                        modifier=Modifier.padding(horizontal=dimensionResource(id=R.dimen.margin_xxlarge)),
+                        thickness=dimensionResource(id=R.dimen.divider_thickness)
                     )
-                }
-                items(items = items) { item ->
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                onClick.invoke(item)
-                                active = false
-                            }
-                            .padding(
-                                horizontal = dimensionResource(id = R.dimen.margin_large),
-                                vertical = dimensionResource(
-                                    id = R.dimen.margin_small
-                                )
-                            ),
-                        text = item.label,
-                        style = MaterialTheme.typography.bodyLarge,
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.primary,
-                        textAlign = TextAlign.Center,
-                    )
-                    if (items.size - 1 != (items.indexOf(item))) {
-                        HorizontalDivider(
-                            modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.margin_xxlarge)),
-                            thickness = dimensionResource(id = R.dimen.divider_thickness)
-                        )
-                    }
                 }
             }
         }
-
     }
 }
